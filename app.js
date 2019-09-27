40e3c2a6ec96239f3fcc97ee37ad34fc460a8677
Nicolas <nicolasgarciacomp@gmail.com>
2019-09-27 20:33:49 +0100

    Primer commit inicial

---
+++ app.js
@@ -0,0 +1,27 @@
+const argv = require('./config/yargs.js').argv;
+const colors = require('colors');
+
+const {crearArchivo, listarTabla} = require('./multiplicar/multiplicar.js');
+
+let comando = argv._[0];
+
+switch(comando) {
+	case 'listar':
+		listarTabla(argv.base, argv.limite);
+	break;
+
+	case 'crear':
+		crearArchivo(argv.base, argv.limite)
+			.then(archivo => console.log(`Archivo creado: ${archivo.green}`))
+			.catch(e => console.log(e));
+	break;
+
+	default:
+		console.log('Comando no reconocido');
+}
+
+let argv2 = process.argv;
+//let parametro = argv[2];
+//let base = parametro.split('=')[1];
+
+//console.log(argv.base);
---
+++ multiplicar/multiplicar.js
@@ -0,0 +1,41 @@
+// Requireds
+const fs = require('fs');
+const colors = require('colors');
+
+let listarTabla = (base, limite = 10) => {
+
+	console.log('###############################'.green);
+	console.log('############ TABLA ############'.green);
+	console.log('###############################'.green);
+
+	for(let i = 0; i <= limite; i++) {
+		console.log(`${base} * ${i} = ${base * i}\n`);
+	}
+}
+
+let crearArchivo = (base, limite = 10) => {
+	return new Promise((resolve, reject) => {
+		let data = '';
+
+		if(!Number(base)) {
+			reject(`El valor introducido ${base} no es un numero`);
+			return;
+		}
+
+		for(let i = 0; i <= limite; i++) {
+			data += `${base} * ${i} = ${base * i}\n`;
+		}
+
+		fs.writeFile(`./tablas/tabla.txt`, data, (err) => {
+			if(err) 
+				reject(err);
+			else
+				resolve(`./tablas/tabla.txt`);
+		});
+	});
+}
+
+module.exports = {
+	crearArchivo,
+	listarTabla
+}
